de242b491028451ff836580cd6a907aa55c0544f
Dafa Armanto <randikadafa07@gmail.com>
2025-03-08 20:35:47 +0700

    Implementing writeClient to View component

--- components/View.tsx
+++ components/View.tsx
@@ -1,12 +1,22 @@
-import { client } from "@/sanity/lib/client";
 import Ping from "./Ping";
+import { unstable_after as after } from "next/server";
+import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { EyeIcon } from "lucide-react";
+import { writeClient } from "@/sanity/lib/write-client";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
