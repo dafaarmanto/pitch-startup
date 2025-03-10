c3ef725ccc960b2e50f9917b8080ede8e7e68749
Dafa Armanto <randikadafa07@gmail.com>
2025-03-10 21:49:54 +0700

    Add editor picks and change the data fetch parallel

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -2,12 +2,16 @@ import Link from "next/link";
 import Image from "next/image";
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import markdownit from "markdown-it";
 import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
 
 type PageParams = {
   params: Promise<{ id: string }>;
@@ -18,7 +22,11 @@ export const experimental_ppr = true;
 
 const Page = async ({ params }: PageParams) => {
   const id = (await params).id;
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks" }),
+  ]);
 
   if (!post) return notFound();
 
@@ -70,6 +78,16 @@ const Page = async ({ params }: PageParams) => {
           )}
         </div>
         <hr className="divider" />
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor picks</p>
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupCardType, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
         </Suspense>
